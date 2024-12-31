f4a492ce6f899043604badaba542277cef172f25
namhyun <namhyun3418@gmail.com>
2024-12-31 14:41:19 +0900

    정올 #588 문제풀이

---
+++ jungol/src/q558/Main.java
@@ -0,0 +1,22 @@
+package q558;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[100];
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = sc.nextInt();
+			if(arr[i] == 0) {
+				sc.close();
+				break;
+			}
+		}
+		for(int i=arr.length-1; i>=0; i--) {
+			if(arr[i] != 0) {
+				System.out.print(arr[i] + " ");
+			}
+		}
+	}
+}
