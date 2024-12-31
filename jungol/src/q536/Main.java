0b850d0122f2f6e8ca42af67d419851ab7cafa37
namhyun <namhyun3418@gmail.com>
2024-12-31 09:22:41 +0900

    정올 #536 문제풀이

---
+++ jungol/src/q536/Main.java
@@ -0,0 +1,18 @@
+package q536;
+
+public class Main {
+	public static void main(String[] args) {
+	// while
+		int i = 1;
+		while(i<=15) {
+			System.out.print(i + " ");
+			i++;
+			// Sytem.out.print(i++ + " ");
+		}
+	/* for
+		for(int j=1; j<=15; j++) {
+			System.out.print(j + " ");
+		}
+	*/
+	}
+}
