d2710536b52c21da178b5cbb5ea066cdd2ca2664
namhyun <namhyun3418@gmail.com>
2025-01-06 17:46:23 +0900

    정올 #9018 문제풀이

--- jungol/src/q9018/Q9018.java
+++ jungol/src/q9018/Q9018.java
@@ -7,15 +7,15 @@ import java.util.Scanner;
 public class Q9018 {
 	public static void main(String[] args) throws UnsupportedEncodingException {
         System.setOut(new PrintStream(System.out, true, "UTF-8"));
-        System.out.println("두 개의 수를 입력하시오. ");
+        System.out.print("두 개의 수를 입력하시오. ");
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         sc.close();
-        System.out.println(a + " + " + b + " = " + (a+b));
-        System.out.println(a + " - " + b + " = " + (a-b));
-        System.out.println(a + " * " + b + " = " + (a*b));
-        System.out.println(a + " / " + b + " = " + (a/b));
-        System.out.println(a + " % " + b + " = " + (a%b));
+        System.out.printf("%d + %d = %d\n", a, b, a+b);
+        System.out.printf("%d - %d = %d\n", a, b, a-b);
+        System.out.printf("%d * %d = %d\n", a, b, a*b);
+        System.out.printf("%d / %d = %d\n", a, b, a/b);
+        System.out.printf("%d %% %d = %d", a, b, a%b);
 	}
 }
