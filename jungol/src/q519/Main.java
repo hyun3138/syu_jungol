51f1c1788e2f3c616d3410d0b43f692c4bc66587
namhyun <namhyun3418@gmail.com>
2025-01-07 09:33:35 +0900

    정올 #519 문제풀이

---
+++ jungol/src/q519/Main.java
@@ -0,0 +1,13 @@
+package q519;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+        System.out.printf("%d %d", a+100, b%10); 
+    }
+}
