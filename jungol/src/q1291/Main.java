9fd3e6ce9142fe8ab399075f88664ba889ae1bdc
SYU <SYU@DESKTOP-1VHBQ5H>
2025-01-10 09:41:50 +0900

    정올 #1291 문제풀이

---
+++ jungol/src/q1291/Main.java
@@ -0,0 +1,37 @@
+package q1291;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int s, e;
+		while(true) {
+			s = sc.nextInt();
+			e = sc.nextInt();
+			if((s<2 || s>9) || (e<2 || e>9)) {
+				System.out.println("INPUT ERROR!");
+			} else {
+				break;
+			}
+		}
+		if(s>e) {
+			for(int i=1; i<=9; i++) {
+				for(int j=s; j>=e; j--) {
+					System.out.printf("%d * %d = %2d", j, i, j*i);
+					System.out.print("   ");
+				}
+				System.out.println();
+			}
+		} else {
+			for(int i=1; i<=9; i++) {
+				for(int j=s; j<=e; j++) {
+					System.out.printf("%d * %d = %2d", j, i, j*i);
+					System.out.print("   ");
+				}
+				System.out.println();
+			}
+		}
+		
+	}
+}
